a4dd99915583d3f9716b2864fc8f7877e8caa3c8
Love Tewari <92184403+lovetewari@users.noreply.github.com>
2022-09-21 21:06:07 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	$host = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "sparksbank";
+        $charset = "utf8mb4";
+
+        $conn = mysqli_connect($host, $user, $pass, $db);
+
+	if(!$conn){
+		die("Unable to connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+?>
+
